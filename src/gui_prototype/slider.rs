4f0d54d37120f349fbbecff5facd65133afc2da7
Kelsey Geiger <kelseyrgeiger@gmail.com>
2025-01-23 15:27:21 -0800

    Defining slider GUI element

--- src/gui_prototype/slider.rs
+++ src/gui_prototype/slider.rs
@@ -16,15 +16,27 @@ pub struct Slider<T>
 }
 
 impl<T> Slider<T>
-    where T: Sized + Add<T> + Mul<T> + Sub<T> + Div<T> + PartialOrd + PartialEq + Copy
+    where T: Sized + Add<T, Output=T> + Mul<T, Output=T> + Sub<T, Output=T> + Div<T, Output=T> + PartialOrd + PartialEq + Copy
 {
     pub fn new(start: (i32, i32), end: (i32, i32), lower_bound: T, upper_bound: T, default_value: Option<T>) -> Slider<T>
     {
+        let initial_val: T = if let Some(val) = default_value { 
+            if val < lower_bound {
+                lower_bound 
+            } else if val > upper_bound { 
+                upper_bound 
+            } else { 
+                val 
+            } 
+        } else { 
+            lower_bound 
+        };
+
         Slider {
             start_pos: Point::new(start.0, start.1),
             end_pos: Point::new(end.0, end.1),
             value_bounds: if lower_bound > upper_bound { (upper_bound, lower_bound) } else { (lower_bound, upper_bound) },
-            value: Cell::new(if let Some(val) = default_value { val } else { lower_bound })
+            value: Cell::new(initial_val)
         }
     }
 }
